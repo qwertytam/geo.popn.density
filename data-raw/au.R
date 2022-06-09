723330e004e8e6c10ef9d97183fc190d328042d3
Tom Marshall <tom.q.marshall@gmail.com>
2022-06-09 13:26:50 -0400

    Name and formatting changes

--- data-raw/au.R
+++ data-raw/au.R
@@ -8,36 +8,37 @@ library(here)
 library(tidyverse)
 library(readxl)
 
-extdata_dir <- "inst/extdata"
+ext_data_dir <- "inst/extdata"
 
 # Download population  estimates and components by SA2, 2020 to 2021
 fname_popn <- "32180DS0001_2020-21.xlsx"
-fpath_popn <- here(extdata_dir, fname_popn)
+fpath_popn <- here(ext_data_dir, fname_popn)
 
 url_base <- "https://www.abs.gov.au/"
-url_popn <- paste(url_base, "statistics/people/population/regional-population/2020-21/", sep="")
-url_popn <- paste(url_popn, fname_popn, sep="")
+url_popn <- paste(url_base,
+                 "statistics/people/population/regional-population/2020-21/",
+                 sep="")
 
 res <- tryCatch(download.file(url_popn, fpath_popn), error = function(e) 1)
 
 # Set up the column names and column types from the downloaded file
 # Note skipping columns that are blank or we are ignoring
 col_names = c("State_Code", "State_Name", "GCCSA_Code", "GCCSA_Name")
-col_types = c("numeric", "text", "text", "text")
+col_types = c("numeric",    "text",       "text",       "text")
 
 col_names = c(col_names, "S4_Code", "S4_Name", "S3_Code", "S3_Name")
-col_types = c(col_types, "numeric", "text", "numeric", "text")
+col_types = c(col_types, "numeric", "text",    "numeric", "text")
 
-col_names = c(col_names, "S2_Code", "S2_Name", "Popn_Est")
+col_names = c(col_names, "S2_Code", "S2_Name",      "Popn_Est")
 col_types = c(col_types, "numeric", "text", "skip", "numeric")
 col_types = c(col_types, "skip", "skip", "skip", "skip", "skip", "skip")
 
-col_names = c(col_names, "Area_km2")
+col_names = c(col_names,                 "Area_km2")
 col_types = c(col_types, "skip", "skip", "numeric", "skip")
 
 # Read in the data and iterate through the relevant tabs
-shts <- c("Table 1", "Table 2", "Table 3", "Table 4", "Table 5")
-shts <- c(shts, "Table 6", "Table 7", "Table 8")
+shts <- c("Table 1", "Table 2", "Table 3", "Table 4", "Table 5", "Table 6",
+          "Table 7", "Table 8")
 
 first_data_row <- 10
 tbl_df <- map_dfr(set_names(shts, shts),
@@ -56,24 +57,26 @@ tbl_df <- tbl_df %>%
 
 # Now get the SA2 to SUA mapping logic
 fname_sa2_sua <- "1270055004_sa2_sua_2016_aust_csv.zip"
-fpath_sa2_sua <- here(extdata_dir, fname_sa2_sua)
+fpath_sa2_sua <- here(ext_data_dir, fname_sa2_sua)
 
-url_sa2_sua <- paste(url_base, "ausstats/subscriber.nsf/", sep="")
-url_sa2_sua <- paste(url_sa2_sua, "log?openagent&1270055004_sa2_sua_2016_aust_csv.zip", sep="")
-url_sa2_sua <- paste(url_sa2_sua, "&1270.0.55.004&Data%20Cubes", sep="")
-url_sa2_sua <- paste(url_sa2_sua, "&D6E51168BD6DC248CA2581B1000E0A48&0", sep="")
-url_sa2_sua <- paste(url_sa2_sua, "&July%202016&09.10.2017&Latest", sep="")
+url_sa2_sua <- paste(url_base,
+                     "ausstats/subscriber.nsf/log?openagent&",
+                     "1270055004_sa2_sua_2016_aust_csv.zip",
+                     "&1270.0.55.004&Data%20Cubes",
+                     "&D6E51168BD6DC248CA2581B1000E0A48&0",
+                     "&July%202016&09.10.2017&Latest", sep="")
 
-res <- tryCatch(download.file(url_sa2_sua, fpath_sa2_sua), error = function(e) 1)
+res <- tryCatch(download.file(url_sa2_sua, fpath_sa2_sua),
+                error = function(e) 1)
 
 # Unzip the download and remove the zip file
 flist <- unzip(fpath_sa2_sua, list = TRUE)
-unzip(fpath_sa2_sua, exdir = here(extdata_dir))
+unzip(fpath_sa2_sua, exdir = here(ext_data_dir))
 if (file.exists(fpath_sa2_sua)){ file.remove(fpath_sa2_sua) }
 
 # Update the file path to point to the first file extracted as we are
 # assuming that that there is only one file in the archive
-fpath_sa2_sua <- here(extdata_dir, flist[[1]])
+fpath_sa2_sua <- here(ext_data_dir, flist[[1]])
 sa2_sua_map <- read_csv(fpath_sa2_sua)
 
 # Join the map to the data
@@ -88,7 +91,7 @@ tbl_df <- tbl_df %>%
 
 # Write the data to a csv file
 fname_out <- "au.csv"
-fpath_out <- here(extdata_dir, fname_out)
+fpath_out <- here(ext_data_dir, fname_out)
 tbl_df %>%
   write_csv(fpath_out)
 
