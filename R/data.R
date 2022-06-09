dbeeae3f69f1f324f2691695a881aa137bb61a46
Tom Marshall <tom.q.marshall@gmail.com>
2022-06-09 13:27:25 -0400

    Add us entry

--- R/data.R
+++ R/data.R
@@ -24,3 +24,29 @@
 #' }
 #'
 "au"
+
+#' US population and population densities at the census 'block group' level.
+#' Each row is a population estimate, area and population density for a census
+#' block group.
+#'
+#' This dataset was produced from US Census Bureau data
+#'  available from  \url{https://www.census.gov/"/}.
+#'
+#' @format A data frame with 241898 rows and 12 variables:
+#' \describe{
+#'   \item{State_FIPS}{State code}
+#'   \item{State_Abbr}{Two letter USPS state abbreviation}
+#'   \item{State_Name}{State name}
+#'   \item{County_FIPS}{County or equivalent FIPS code}
+#'   \item{County_Name}{County or equivalent name}
+#'   \item{CBSA_Code}{Core Based Statistical Area code}
+#'   \item{CBSA_Node}{Core Based Statistical Area name}
+#'   \item{Metro_Micro}{If the block group is in a metro or micropolitan area}
+#'   \item{S3_Name}{Statistical Area level 3 name}
+#'   \item{GEOID}{GEOID of block group?}
+#'   \item{Popn_Est}{Population estimate, currenlty from the 2020 census}
+#'   \item{Area_km2}{Area of the relevant block group}
+#'   \item{Density}{Derived density}
+#' }
+#'
+"us"
