266f694868f6f4687f7cbd1065679181d9a179c2
Tom Marshall <tom.q.marshall@gmail.com>
2022-06-16 10:22:48 -0400

    Documentation and import update

---
+++ R/geo.popn.density.R
@@ -0,0 +1,9 @@
+#' Find Population Densities For Selected Countries
+#'
+#' Finds the population densities for selected countries. Gets and
+#' wrangles the underlying source data, then calculates simple and weighted
+#' averge densities.
+#'
+#' @docType package
+#' @name geo.popn.density
+#' 
