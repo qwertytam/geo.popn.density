4d65e1c53b492bd4024104e8941ebc5afc35f503
Tom Marshall <tom.q.marshall@gmail.com>
2022-06-17 23:17:10 -0400

    debugging

--- R/plots.R
+++ R/plots.R
@@ -131,7 +131,7 @@ PlotMSAsByPopn <- function(msas_by_popn,
 #'   \item{method}{Calculation density}
 #' }
 #' @importFrom magrittr %>%
-#' @importFrom rlang sym
+#' @importFrom rlang sym .data
 #' @export
 #'
 PlotMSAsByDensity <- function(msas_by_density,
